f87e70da3c15c0a502f7d21a1d8810da137d6a2a
Matthew Li <matthewli2003@gmail.com>
2021-07-11 14:26:12 -0700

    Added checkout page and headers.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,6 +11,9 @@ import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import { auth } from "../../firebase/firebase.utils";
+import { createStructuredSelector } from "reselect";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selector";
 
 import "./header.styles.scss";
 
@@ -26,7 +29,7 @@ const Header = ({ currentUser, hidden }) => (
 			<Link className='option' to='/shop'>
 				CONTACT
 			</Link>
-			{currentUser ? (
+			{currentUser && (currentUser.userAuth || currentUser.email) ? (
 				<div className='option' onClick={() => auth.signOut()}>
 					SIGN OUT
 				</div>
@@ -41,9 +44,9 @@ const Header = ({ currentUser, hidden }) => (
 	</div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-	currentUser,
-	hidden,
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser,
+	hidden: selectCartHidden,
 });
 
 export default connect(mapStateToProps)(Header);
