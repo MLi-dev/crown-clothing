55672771f1307d683a6a5a10c418192fd8b48843
Matthew Li <matthewli2003@gmail.com>
2021-07-22 21:26:51 -0700

    Added wrapper for shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,9 +8,17 @@ import {
 	convertCollectionSnapShotToMap,
 } from "../../firebase/firebase.utils";
 
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
 import { updateCollections } from "../../redux/shop/shop.actions";
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+	state = {
+		loading: true,
+	};
 	unsubscribeFromSnapShot = null;
 
 	componentDidMount() {
@@ -21,17 +29,27 @@ class ShopPage extends React.Component {
 			async (snapshot) => {
 				const collectionsMap = convertCollectionSnapShotToMap(snapshot);
 				updateCollections(collectionsMap);
+				this.setState({ loading: false });
 			}
 		);
 	}
 	render() {
 		const { match } = this.props;
+		const { loading } = this.state;
 		return (
 			<div className='shop-page'>
-				<Route exact path={`${match.path}`} component={CollectionsOverview} />
+				<Route
+					exact
+					path={`${match.path}`}
+					render={(props) => (
+						<CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+					)}
+				/>
 				<Route
 					path={`${match.path}/:collectionId`}
-					component={CollectionPage}
+					render={(props) => (
+						<CollectionPageWithSpinner isLoading={loading} {...props} />
+					)}
 				/>
 			</div>
 		);
